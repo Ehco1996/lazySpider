38e36dab04a09f5fbdb4de840e018e442815f612
ehco1996 <zh19960202@gmail.com>
2017-12-19 15:04:05 +0800

    优化打包配置文件

--- setup.py
+++ setup.py
@@ -1,28 +1,36 @@
 from setuptools import setup
 
+VERSION = '0.0.1'
+URL = 'https://github.com/Ehco1996/lazySpider'
+KEYWORDS = 'spider headers mysql'
+EMAIL = 'zh19960202@gmial.com'
+DESCRIPTION = "Powerful spider tools"
+LONG_DESCRIPTION = '''
+                    lazyspider is a powerful tools which can: 
+                    help you get request header/cookie easily
+                    help you manipulate database in humanized way
+                    '''
+REQUIRES = ['pymysql']
+PACKAGES = ['lazyspider']
+
 setup(
     name='lazyspider',
-    version='0.0.1',
-    description='Powerful spider tools',
-    long_description=''' 
-    lazyspider is a powerful tools which can:
-        help you get request header/cookie easily
-        help you manipulate database in humanized way
-    ''',
-    url='https://github.com/Ehco1996/lazySpider',
     author='Ehco1996',
-    author_email='zh19960202@gmial.com',
     license='GPLv3',
-    keywords='spider headers mysql',
     zip_safe=False,
-    install_requires=['pymysql'],
+    url=URL,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author_email=EMAIL,
+    keywords=KEYWORDS,
+    install_requires=REQUIRES,
+    packages=PACKAGES,
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Natural Language :: zh-cn',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    packages=['lazyspider'],
 )
